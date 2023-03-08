19a33e7a75be2e76e069fb80c07d1a6c51bb5518
itadown <itadown@itadown>
2023-03-08 14:49:08 +0100

    Enum Equipement et sEquiper méthode Romain

--- LesGaulois/src/personnages/Equipement.java
+++ LesGaulois/src/personnages/Equipement.java
@@ -1,5 +1,18 @@
 package personnages;
 
-public enum Equipement {
-	CASQUE, BOUCLIER;
+public enum Equipement 
+{
+	CASQUE("casque"), BOUCLIER("bouclier");
+	
+	private String nom;
+	
+	 Equipement(String nom)
+	{
+		this.nom = nom;
+	}
+	
+	public String toString()
+	{
+		return nom;
+	}
 }
--- LesGaulois/src/personnages/Romain.java
+++ LesGaulois/src/personnages/Romain.java
@@ -3,12 +3,42 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement [] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) 
 	{
 		assert force > 0; 
 		this.nom = nom;
 		this.force = force;
+	}
+	
+	public void sEquiper(Equipement equipement)
+	{	
+		switch(nbEquipement)
+		{
+			case 2:
+				System.out.println("Le soldat " + nom + " est deja bien protege !");
+				break;
+			case 1:
+				if (equipements[0] == equipement)
+				{
+					System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
+				}
+				else
+				{			
+					equipements[nbEquipement] = equipement;
+					nbEquipement++;
+					System.out.println("Le soldat " + nom + " s'equipe avec un " + equipement + ".");
+				}
+				break;
+			default:
+				equipements[nbEquipement] = equipement;
+				nbEquipement++;
+				System.out.println("Le soldat " + nom + " s'equipe avec un " + equipement + ".");
+				break;
+		}
+		
 	}
 	
 	public String getNom() 
@@ -50,6 +80,9 @@ public class Romain {
 		
 		Romain minus = new Romain("Minus", 6);
 		
-		
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 	}
 }
